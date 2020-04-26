6cfcf50100e35b91e065fe8aa50b9f249127108d
SemteulGaram <scgtdy7151@gmail.com>
2020-04-26 21:44:44 +0900

    Fix: complete silence mode

--- install.sh
+++ install.sh
@@ -5,7 +5,8 @@ echo "Target chatroomid (ex. 123456789)>"
 read chatroomId
 
 url="https://api.telegram.org/bot${botId}/sendMessage?chat_id=${chatroomId}&text=ALERT - Server Shell Access - ${USER}%40${HOSTNAME}"
-echo "curl -s -m 10 \"${url}\" 2> /dev/null > /dev/null &" >> ~/.bashrc
+command="{ curl -s -m 10 \"${url}\" 2> /dev/null > /dev/null & } 2>/dev/null;disown &>/dev/null"
+echo $command >> ~/.bashrc
 
-echo $url
+echo $command
 echo "Successful write script in ~/.bashrc"
