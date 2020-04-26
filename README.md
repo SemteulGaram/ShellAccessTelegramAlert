9a3fa5d9d4970ba0a669ee8e2351a1c4a6b19c44
SemteulGaram <scgtdy7151@gmail.com>
2020-04-26 20:50:53 +0900

    Fix: some typo

--- README.md
+++ README.md
@@ -12,7 +12,7 @@
 1. Login user you want to apply
 2. `git clone https://github.com/SemteulGaram/ShellAccessTelegramAlert.git` 
 3. `cd ShellAccessTelegramAlert`
-5. 'chmod +x install.sh'
+5. `chmod +x install.sh`
 6. `./install.sh`
 7. The script asks the following: Telegram botId, target chatroomId
 
--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ echo "Target chatroomid (ex. 123456789)>"
 read chatroomId
 
 url="https://api.telegram.org/bot${botId}/sendMessage?chat_id=${chatroomId}&text=ALERT - Server Shell Access - ${USER}%40${HOSTNAME}"
-echo "curl -m 10 ${url} > /dev/null" >> ~/.bashrc
+echo "curl -s -m 10 \"${url}\" 2> /dev/null > /dev/null" >> ~/.bashrc
 
 echo $url
 echo "Successful write script in ~/.bashrc"
